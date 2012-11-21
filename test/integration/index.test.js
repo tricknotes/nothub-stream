a6d6f94af4e4863627c745a373509188c2f166a0
Ryunosuke SATO <tricknotes.rs@gmail.com>
2012-11-22 02:16:28 +0900

    Rewrite integration test using Socket.IO

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -1,10 +1,10 @@
 var nock = require('nock')
   , expect = require('chai').expect
-  , WebSocket = require('ws')
+  , io = require('socket.io-client')
   , NotHubStream = require('../../')
   , Crawler = NotHubStream.Crawler
   , Server = NotHubStream.Server
-  , WebSocketListener = NotHubStream.Listeners.WebSocketListener
+  , SocketIOListener = NotHubStream.Listeners.SocketIOListener
 
 describe('NotHub Stream', function() {
   var crawler = null
@@ -15,7 +15,7 @@ describe('NotHub Stream', function() {
   beforeEach(function(done) {
     crawler = new Crawler();
     server = new Server();
-    listener = new WebSocketListener({ port: ++port });
+    listener = new SocketIOListener(++port, { log: false });
     listener.listen(server, done);
     crawler.on('receive', function(err, data) {
       server.send(data);
@@ -31,60 +31,74 @@ describe('NotHub Stream', function() {
   });
 
   it('should receive all data without query', function(done) {
-    var ws = new WebSocket('ws://localhost:' + port);
-    ws.on('open', function() {
-      ws.send(JSON.stringify({ type: 'query' }));
-      process.nextTick(function() {
-        crawler.fetch();
-      });
+    var socket = io.connect('http://localhost:' + port);
+    socket.on('connect', function() {
+      socket.emit('query', {});
+      crawler.fetch();
     });
-    ws.on('message', function(data) {
-      expect(data).to.eql('{"type":"OK"}');
+    socket.on('gh_event pushed', function(data) {
+      expect(data).to.eql({ type: 'OK' });
       done();
     });
   });
 
   it('should receive data matched to own query', function(done) {
-    var ws = new WebSocket('ws://localhost:' + port);
-    ws.on('open', function() {
-      listener.once('query-update', function(err, query) {
+    var socket = io.connect('http://localhost:' + port);
+    socket.on('connect', function() {
+      listener.once('query-update', function(error, id, query) {
         expect(query).to.eql({ type: 'NG' });
         process.nextTick(function() {
           crawler.fetch();
         });
-        listener.once('query-update', function(err, query) {
+        listener.once('query-update', function(error, id, query) {
           expect(query).to.eql({ type: 'OK' });
           crawler.fetch();
         });
-        ws.send(JSON.stringify({ type: 'query', query: { type: 'OK' } }));
+        socket.emit('query', { type: 'OK' } );
       });
-      ws.send(JSON.stringify({ type: 'query', query: { type: 'NG' } }));
+      socket.emit('query', { type: 'NG' } );
     });
-    ws.on('message', function(data) {
-      expect(data).to.eql('{"type":"OK"}');
+    socket.on('gh_event pushed', function(data) {
+      expect(data).to.eql({ type: "OK" });
       done();
     });
   });
 
   it('should receive data own interested', function(done) {
-    var ws1 = new WebSocket('ws://localhost:' + port);
-    ws1.on('open', function() {
-      ws1.send(JSON.stringify({ type: 'query', query: { type: 'NG' } }));
-      listener.once('query-update', function(err, query) {
-        var ws2 = new WebSocket('ws://localhost:' + port);
-        ws2.on('open', function() {
-          ws2.send(JSON.stringify({ type: 'query', query: { type: 'OK' } }));
-          listener.once('query-update', function(err, query) {
-            crawler.fetch();
-          });
-        });
-        ws2.on('message', function() {
-          done();
-        });
-      });
-    });
-    ws1.on('message', function() {
+    var socket1 = io.connect('http://localhost:' + port);
+    var listener2 = new SocketIOListener(++port, { log: false });
+    listener2.listen(server);
+    var socket2 = io.connect('http://localhost:' + port);
+
+    socket1.on('gh_event pushed', function() {
       throw new Error('This listener should not be called.');
+    });
+    socket2.on('gh_event pushed', function() {
+      done();
+    });
+
+    var connected = (function() {
+      var connetedClientCount = 0;
+
+      return function(callback) {
+        connetedClientCount += 1;
+        if (connetedClientCount == 2) {
+          callback();
+        }
+      };
+    })();
+
+    var startAssertion = function() {
+      socket1.emit('query', { type: 'NG' });
+      socket2.emit('query', { type: 'OK' });
+      crawler.fetch();
+    };
+
+    socket1.on('connect', function() {
+      connected(startAssertion);
+    });
+    socket2.on('connect', function() {
+      connected(startAssertion);
     });
   });
 });
